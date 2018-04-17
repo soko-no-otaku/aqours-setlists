f6138f8fc549fbe7449b171871b588313e53953f
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-17 23:32:57 +0900

    Song, Eventモデル間のアソシエーションの記述

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :songs, through: :event_songs
+  has_many :event_songs
+  accepts_nested_attributes_for :event_songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ApplicationRecord
+  has_many :events, through: :event_songs
+  has_many :event_songs
 end
