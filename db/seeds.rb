f9b3c5fe0aa80014c9e2dffad822944385a1d292
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-18 00:08:50 +0900

    seeds.rbに仮の初期データを記載

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first = Event.create(title: 'Aqours First LoveLive! ～Step! ZERO to ONE～ Day.1', started_at: '2017-02-25 17:30')
+first.songs.create(title: '青空Jumping Heart', released_at: '2016-07-20')
+first.songs.create(title: '恋になりたいAQUARIUM', released_at: '2016-04-27')
