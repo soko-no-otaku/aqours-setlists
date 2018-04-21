7e9de8d637b884f2d5218e38a42e52f2cbf9c203
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-22 00:35:30 +0900

    これまでに披露された回数を表示

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,10 @@ class Song < ApplicationRecord
   has_many :event_songs
   has_many :events, through: :event_songs
 
+  def performed_count
+    event_songs.size
+  end
+
   def last_performed_event
     events.order('started_at').last
   end
