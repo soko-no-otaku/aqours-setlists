35b4398b7410b1c222b0a93fa753b91660e6b12e
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-22 00:24:26 +0900

    最後に披露された日を表示

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,18 @@
 class Song < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   has_many :event_songs
   has_many :events, through: :event_songs
+
+  def last_performed_event
+    events.order('started_at').last
+  end
+
+  def last_perfomed_time_ago_in_words
+    unless last_performed_event.nil?
+      time_ago_in_words(last_performed_event.started_at)
+    else
+      '未披露'
+    end
+  end
 end
