a626e9d001bc5b21b308c5fe62029f982ba303f4
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-30 20:46:50 +0900

    前後のイベントへのリンクを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ApplicationRecord
   has_many :event_songs, -> { order(position: :asc) }, inverse_of: :event
   has_many :songs, through: :event_songs
   accepts_nested_attributes_for :event_songs, allow_destroy: true
+
+  def previous
+    Event.order(started_at: :desc, id: :desc).where('started_at <= ? and id < ?', started_at, id).first
+  end
+
+  def next
+    Event.order(started_at: :desc, id: :desc).where('started_at >= ? and id > ?', started_at, id).last
+  end
 end
