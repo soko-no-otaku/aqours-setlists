c6314cfec96fa46ea23b998ef21eeb052a64d367
soko-no-otaku <soko.no.otaku@gmail.com>
2018-04-30 23:05:33 +0900

    各イベントでの曲数の表示

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,10 @@ class Event < ApplicationRecord
   has_many :songs, through: :event_songs
   accepts_nested_attributes_for :event_songs, allow_destroy: true
 
+  def songs_count
+    event_songs.size
+  end
+
   def previous
     Event.order(started_at: :desc, id: :desc).where('started_at <= ? and id < ?', started_at, id).first
   end
