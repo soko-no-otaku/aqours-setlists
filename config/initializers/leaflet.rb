ed87b62ef5419315f06619ad6c24e0e3cb693977
soko-no-otaku <soko.no.otaku@gmail.com>
2018-05-16 01:10:53 +0900

    leafletの初期化コードを追加

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+Leaflet.tile_layer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = 'Your attribution statement'
+Leaflet.max_zoom = 18
