36e7892b655431a7ad155f73b590b9fc4a118566
soko-no-otaku <soko.no.otaku@gmail.com>
2018-05-27 23:05:57 +0900

    showとindex以外のアクションをルーティングから除外

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :venues
+  resources :venues, only: [:show, :index]
   root :to => 'home#index'
 
   get 'cooccurrence_graph', to: 'cooccurrence_graph#index'
   get 'cooccurrence_graph/data', to: 'cooccurrence_graph#data'
 
-  resources :events
-  resources :songs
+  resources :events, only: [:show, :index]
+  resources :songs, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
