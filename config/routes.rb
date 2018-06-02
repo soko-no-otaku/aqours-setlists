cdd0932da5d670257b9ba339d5fbd13de66a3e28
soko-no-otaku <soko.no.otaku@gmail.com>
2018-06-02 20:05:08 +0900

    作編曲者グラフ用のルーティングとリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'cooccurrence_graph', to: 'cooccurrence_graph#index'
   get 'cooccurrence_graph/data', to: 'cooccurrence_graph#data'
 
+  get 'composers_graph', to: 'composers_graph#index'
+  get 'composers_graph/data', to: 'composers_graph#data'
+
   resources :events, only: [:show, :index]
   resources :songs, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
