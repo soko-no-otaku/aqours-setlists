ba378f3a563984f89832d2229d19426cd4bec9d6
soko-no-otaku <soko.no.otaku@gmail.com>
2018-10-17 23:32:30 +0900

    eventsをseed-fuに移行

---
+++ db/fixtures/03_events.rb
@@ -0,0 +1,31 @@
+YAML.load_file('db/events.yml').each.with_index(1) do |event, i|
+  Event.seed do |s|
+    s.id = i
+    s.title = event[:title]
+    s.tag_list = event[:tags]
+  end
+
+  event[:setlists].each do |setlist|
+    Setlist.seed do |s|
+      s.day_n = setlist[:day_n]
+      s.started_at = setlist[:started_at]
+      s.event = Event.find(i)
+    end
+
+    setlist[:setlist].each do |setlist_song|
+      SetlistSong.seed do |s|
+        s.setlist = Setlist.find_by(day_n: setlist[:day_n], event_id: i)
+        s.song = Song.find_by(title: setlist_song[:title])
+        s.available_in_video = setlist_song[:available_in_video]
+        s.encore = setlist_song[:encore]
+        s.size = setlist_song[:size]
+        s.costume_list = setlist_song[:costumes]
+      end
+    end
+  end
+
+  EventVenue.seed do |s|
+    s.event = Event.find(i)
+    s.venue = Venue.find_by(name: event[:venue])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-YAML.load_file('db/events.yml').each do |e|
-  event = Event.create(title: e[:title])
-
-  e[:setlists].each do |sl|
-    setlist = Setlist.create(
-        day_n: sl[:day_n],
-        started_at: sl[:started_at],
-        event: event
-    )
-
-    sl[:setlist].each do |s|
-      song = Song.find_by(title: s[:title])
-      setlist_song = SetlistSong.create(
-          setlist: setlist,
-          song: song,
-          available_in_video: s[:available_in_video],
-          encore: s[:encore],
-          size: s[:size]
-      )
-
-      setlist_song.costume_list = s[:costumes]
-      setlist_song.save
-    end
-  end
-
-  venue = Venue.find_by(name: e[:venue])
-  EventVenue.create(
-      event: event,
-      venue: venue
-  )
-
-  event.tag_list = e[:tags]
-  event.save
-end
