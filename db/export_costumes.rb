eda0d1849bd05945c82280a65e3fe2c7fdf1fe7d
soko-no-otaku <21125932+soko-no-otaku@users.noreply.github.com>
2023-03-21 16:50:54 +0900

    Add event_id

--- db/export_costumes.rb
+++ db/export_costumes.rb
@@ -3,11 +3,11 @@ require 'yaml'
 require 'csv'
 
 CSV.open('costumes.csv', 'wb') do |csv|
-  YAML.load_file('events.yml').each do |event|
+  YAML.load_file('events.yml').each.with_index(1) do |event, event_id|
     event[:setlists].each do |setlist|
       setlist[:setlist].each do |setlist_song|
         title = setlist_song[:title]
-        row = [title, *setlist_song[:costumes]]
+        row = [event_id, title, *setlist_song[:costumes]]
         csv << row
       end
     end
